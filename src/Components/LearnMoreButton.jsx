aa0a6cfb1d50c8c0eac975e123f002761e133612
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-09-22 21:41:18 +0200

    Improved homepage

--- src/Components/LearnMoreButton.jsx
+++ src/Components/LearnMoreButton.jsx
@@ -8,7 +8,7 @@ export default function LearnMoreButton() {
         <div className="w-fit hover:rounded-lg p-2 hover:bg-gradient-to-r from-primary-500 to-secondary-500 text-primary-500 hover:text-background-50">
         <Link to="/about" className="hover:text-background-50">
         <div className="flex justify-center items-center gap-2 hover:text-background-50">
-            <p className='text-center text-xl md:text-2xl'>Learn More</p>
+            <p className='text-center text-xl md:text-2xl'>About Me</p>
             <VscArrowRight className="text-2xl"/>
         </div>
         </Link>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import georgesImage from "../assets/georges.jpeg";
 import LearnMore from "../Components/LearnMoreButton";
 import ExploreProjectsButton from "../Components/ExploreProjectsButton";
+import { IoMail } from "react-icons/io5";
+
 
 // import ProjectsGrid from "../Components/ProjectsGrid";
 
@@ -40,15 +42,19 @@ const Home = () => {
             className="rounded-full h-40 w-40"
           />
         </div>
-        <p className="mt-8 mb-10 text-base md:text-lg text-text-950 text-justify">
-        Hi! I'm Georges, a Computer Science MSc living in London. I have a passion for technology and innovation. 
-        My expertise spans Python, SQL, and modern web technologies like React and Next.js. 
+        <p className="mt-8 mb-4 text-base md:text-lg text-text-950 text-justify">
+        Hi! I'm Georges, a Computer Science MSc graduate living in London. I have a passion for technology and innovation. 
+        My expertise spans modern web technologies like React and Next.js, as well as Python. 
         I've tackled diverse projects, and am always eager to learn more.
         Explore my portfolio to see how I bridge the gap between code and creative solutions.
         </p>
+        <div className="flex flex-row gap-2 items-center mb-8 ">
+        <IoMail size={20}/> 
+        <p className="text-base md:text-lg text-text-950 font-semibold">georges.lnl01@gmail.com</p>
+        </div>
         <div className="flex flex-row justify-around lg:flex lg:flex-col lg:gap-2">
-        <LearnMore />
         <ExploreProjectsButton />
+        <LearnMore />
         </div>
         {/* <div className="flex flex-col items-center mt-16">
       <ProjectsGrid projects={projects} />
