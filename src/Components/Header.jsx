e061a59bf36b9b0d665b96a8161f348c6f86d1bc
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-09-23 15:44:35 +0200

    Put icons on top left

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./NavBar";
-import TopLeftIcons from "./HomeIcon";
+import TopLeftIcons from "./TopLeftIcons";
 
 export default function Header({darkMode, toggleDarkMode}) {
     return (
