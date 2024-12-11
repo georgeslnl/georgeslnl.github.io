488e1ad935f4bf955ccb6f5b4f918bd7fbd198a0
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-12-11 21:05:50 +0100

    Add project details for Maya-Aska portfolio including abstract, skills, and links

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -14,6 +14,12 @@ const projectsData = [
     title: "Maya-Aska Portfolio",
     date: "2024 - now",
     description: "Portfolio website for a photographer.",
+    abstract:
+        "Maya-Aska is a London-based photographer. She requested a portfolio website to showcase her work. \n\n\n\nA sleek and elegant design was chosen to highlight her photography.",
+    skills: ["NextJS", "Tailwind CSS", "JavaScript"],
+    github: "https://github.com/georgeslnl/maya-aska-portfolio",
+    deployedLink: "https://maya-aska-portfolio-git-development-georges-projects-d90b908b.vercel.app/",
+
   },
   {
     id: "ui-copilot",
