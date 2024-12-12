cb511c4683ad0897352c1907b157d3473df72945
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-12-12 17:37:36 +0100

    FIX: Routing issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 import ScrollToTop from "./Components/ScrollToTop";
